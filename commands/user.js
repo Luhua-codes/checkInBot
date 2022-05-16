798fbba4e20fc7129175ac09561966084b4be8f5
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-05-16 03:23:42 -0400

    removed user specification option from user command

--- commands/user.js
+++ commands/user.js
@@ -3,12 +3,10 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Replies with user avatar link and tag')
-		.addUserOption(option =>
-			option.setName('user tag')
-				.setDescription('The user to return information about')
-				.setRequired(false)),
+		.setDescription('Replies with user avatar link and tag'),
 	async execute(interaction) {
-		await interaction.reply(`${interaction.user.avatarURL()}\n**User tag**: ${interaction.user.tag}`);
+		await interaction.reply(`${interaction.user.avatarURL()}\n**Your tag**: ${interaction.user.tag}`);
 	},
 };
+
+//TODO later: add option to specify user to provide info on https://discordjs.guide/interactions/slash-commands.html#options
