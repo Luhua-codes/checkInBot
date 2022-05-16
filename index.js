5bec5499e6f5af454c605d06f4a0d4a105080bde
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-05-16 02:52:45 -0400

    setup new requirements for index.js

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json'); //module.exports used export data in Node.js so you can require() it in other files
+const fs = require('node:fs'); //node native file system module
+const path = require('node:path'); //node native path utility module
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
+//NOTE: module.exports used export data in Node.js so you can require() it in other files
 
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-// When the client is ready, run this code (only once)
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] }); // Create a new client instance
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('Ready!'); //client is ready
 });
+
+client.commands = new Collection(); //allows access to commands in other files
 
 client.on('interactionCreate', async interaction => { //listen for interactions
 	if (!interaction.isCommand()) return; //leave method if not command
-
 	const { commandName } = interaction;
-
-	if (commandName === 'ping') { //strict equality, always considers objs of different types to be !=
-		await interaction.reply('Pong!');
-	}else if(commandName === 'beep'){
-		await interaction.reply("Boop!");
-	}
-	else if (commandName === 'server') {
-		await interaction.reply(`**Server name**: ${interaction.guild.name}\n**Total members**: ${interaction.guild.memberCount}\n**Server created on**: ${interaction.guild.createdAt}\n**Server description**: ${interaction.guild.description == null ? 'No description set' : interaction.guild.description}`); //backtick instead of single quotes allows use of template strings
-	}
-	else if (commandName === 'user') {
-		await interaction.reply(`${interaction.user.avatarURL()}\n**Your tag**:${interaction.user.tag}`);
-	}
 });
 
 // Login to Discord with your client's token
