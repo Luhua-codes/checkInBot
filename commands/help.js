7dc0a2d5b1d06f99fa7c4fb80d34363bc02a3017
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-05-16 04:53:30 -0400

    Set help command to send as embed

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Replies with link to bot guide'),
 	async execute(interaction) {
-		await interaction.reply('https://github.com/Luhua-codes/checkInBot/blob/main/README.md#commands');
+		const commandsList = new MessageEmbed()
+			.setTitle('Commands List')
+			.setURL('https://github.com/Luhua-codes/checkInBot/blob/main/README.md#commands')
+			.addFields(
+				{name: '/command1', value:'descript1'},
+				{name: '/command2', value:'descript2'})
+			.setTimestamp();
+
+		await interaction.reply({embeds: [commandsList]});
 	},
 };
+
+//TODO: Update list of commands
