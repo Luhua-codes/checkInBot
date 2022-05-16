75e6cabc2092e37c8d192eb8eb446517d326095c
JJ-Noirfos <53491467+JJ-Chang@users.noreply.github.com>
2022-05-16 03:21:55 -0400

    add option to specify user for user command

--- commands/user.js
+++ commands/user.js
@@ -3,8 +3,12 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
-		.setDescription('Replies with user avatar link and tag'),
+		.setDescription('Replies with user avatar link and tag')
+		.addUserOption(option =>
+			option.setName('user tag')
+				.setDescription('The user to return information about')
+				.setRequired(false)),
 	async execute(interaction) {
-		await interaction.reply(`${interaction.user.avatarURL()}\n**Your tag**:${interaction.user.tag}`);
+		await interaction.reply(`${interaction.user.avatarURL()}\n**User tag**: ${interaction.user.tag}`);
 	},
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -5,16 +5,18 @@ const { Routes } = require('discord-api-types/v9');
 const { clientId, guildId, token } = require('./config.json');
 
 const commands = [];
-const commandsPath = path.join(__dirname, 'commands'); //get path to commands folder
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); //get all .js files from commmands directory
-for(const file of commandFiles){
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
 	const command = require(filePath);
 	commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
-
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
+
+//TODO later: global deployment https://discordjs.guide/interactions/slash-commands.html#global-commands
